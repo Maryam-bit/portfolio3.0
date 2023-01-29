e3d50f6fa67482c9d556b02ff73dc735071ec4c7
Maryam-bit <rajputmaryam5555@gmail.com>
2023-01-30 00:13:59 +0500

    WIP: blog post section

---
+++ components/BlogPosts.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <BlogPost />
+        <BlogPost />
+        <BlogPost />
+        <BlogPost />
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
