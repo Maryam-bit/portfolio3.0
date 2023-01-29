5342bf7d01c21ef6a9d3b338a3c49d68f820481b
Maryam-bit <rajputmaryam5555@gmail.com>
2023-01-29 23:36:23 +0500

    WIP: navigation

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <!-- <ContentNavigation v-slot="{ navigation }">
+      <ul>
+        <li v-for="link of navigation" :key="link._path">
+          <NuxtLink :to="link._path">{{ link.title }}</NuxtLink>
+        </li>
+      </ul>
+    </ContentNavigation> -->
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  </div>
+</template>
