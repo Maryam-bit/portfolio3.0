b6d912f91920e4057430f1ab755db32867ca52d8
Maryam-bit <rajputmaryam5555@gmail.com>
2023-01-30 00:13:24 +0500

    fix: main pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="w-full flex flex-col items-center h-screen overflow-hidden py-3">
-    <div class="sm:w-2/6">
-      <header class="h-12 sm:full bg-slate-200 border border-slate-300 flex items-center justify-between px-10 text-md">
+  <div class="w-full flex flex-col items-center h-screen py-3">
+    <div class="w-11/12 sm:w-[45rem]">
+      <header class="h-12 sm:full border bg-gray-100 border-gray-200 flex items-center justify-between px-10 text-md">
             <NuxtLink to="/">Home</NuxtLink>
             <NuxtLink to="/about2">About</NuxtLink>
             <NuxtLink to="/about2">Blogs</NuxtLink>
             <NuxtLink to="/about2">Projects</NuxtLink>
     </header>
-    <main class="flex-grow w-full h-full p-4 mx-auto max-w-7xl">
+    <main class="flex-grow w-full h-full pt-4 mx-auto">
       <slot />
     </main>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,15 @@
         </li>
       </ul>
     </ContentNavigation> -->
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <h2 class="text-5xl font-normal">Maryam Noor</h2>
+    <h2 class="text-2xl font-extralight text-gray-400 my-3">Polyglot engineer</h2>
+    <div class="flex flex-row gap-7">
+        <img src="../assets/imgs/envelope-solid.svg" width="23">
+        <img src="../assets/imgs/github.svg" width="23">
+        <img src="../assets/imgs/linkedin.svg" width="23">
+    </div>
+    <h2 class="text-3xl font-thin mt-8">Recent Post</h2>
+    <hr class="mb-7">
+    <BlogPosts/>
   </div>
 </template>
