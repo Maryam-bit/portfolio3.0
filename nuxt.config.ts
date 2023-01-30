4b79b6702985cec03150ecdc653ea41e0a0f5b65
Maryam-bit <rajputmaryam5555@gmail.com>
2023-01-31 04:53:39 +0500

    feat: SEO and meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+          charset: 'utf-16',
+          viewport: 'width=500, initial-scale=1',
+          title: 'Maryam Noor',
+          meta: [
+            // <meta name="description" content="My amazing site">
+            { name: 'description', content: 'Portfolio site' }
+          ],
+
+        }
+    },
     modules: ['@nuxt/content'],
     content: {
         highlight: {
